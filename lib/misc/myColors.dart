4b071d400b41d16e08f7681024af38543635fa77
Marcin Zemlok <marcinzemlok@gmail.com>
2023-09-30 18:38:10 +0200

    Adding MyColors class

---
+++ lib/misc/myColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const gray = Color(0xFFBBBDA9);
+  static const green = Color(0xFFCBD85C);
+  static const orange = Color(0xFFEBC232);
+}
