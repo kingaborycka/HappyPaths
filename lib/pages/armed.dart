bda200ce73c532a73e4de3b74796d18770747b46
Kinga <kingaborycka@wp.pl>
2023-09-30 18:40:50 +0200

    timer

--- lib/pages/armed.dart
+++ lib/pages/armed.dart
@@ -1,16 +1,36 @@
-
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import '../widgets/timer_widget.dart';
+
 class Armed extends StatelessWidget {
-const Armed({ Key? key }) : super(key: key);
+  const Armed({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Armed"),
-      ),
+        appBar: appBar(),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 300,
+              ),
+              TimerWidget(),
+              SizedBox(
+                height: 50,
+              ),
+            ],
+          ),
+        ));
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(""),
+      toolbarHeight: 100,
+      backgroundColor: Colors.grey[300],
     );
   }
 }
---
+++ lib/widgets/timer_widget.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:src/pages/armed.dart';
+
+class TimerWidget extends StatefulWidget {
+  const TimerWidget({Key? key}) : super(key: key);
+
+  @override
+  _TimerWidgetState createState() => _TimerWidgetState();
+}
+
+class _TimerWidgetState extends State<TimerWidget> {
+  Timer? myTimer;
+  Duration myDuration = Duration(hours: 1, minutes: 10);
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  void startTimer() {
+    myTimer = Timer.periodic(Duration(seconds: 1), (_) => setCountDown());
+  }
+
+  void stopTimer() {
+    setState(() => myTimer!.cancel());
+  }
+
+  void setCountDown() {
+    final reduceSecondsBy = 1;
+    setState(() {
+      final seconds = myDuration.inSeconds - reduceSecondsBy;
+      if (seconds < 0) {
+        myTimer!.cancel();
+      } else {
+        myDuration = Duration(seconds: seconds);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String strDigits(int n) => n.toString().padLeft(2, '0');
+    // Step 7
+    final hours = strDigits(myDuration.inHours.remainder(24));
+    final minutes = strDigits(myDuration.inMinutes.remainder(60));
+    final seconds = strDigits(myDuration.inSeconds.remainder(60));
+    return Text(
+      '$hours:$minutes:$seconds',
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.black, fontSize: 70),
+    );
+  }
+}
